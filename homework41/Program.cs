f4ee802b6905d2a8bf3aff8e84e3ff9af86a0db2
Dmitriy <118912417+Lamert2022@users.noreply.github.com>
2022-12-23 15:36:23 +0300

    first commit

---
+++ homework41/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+*/
+Console.WriteLine("Введите числа через пробел");
+var str = Console.ReadLine() ?? "";
+
+int[] arr = Array.ConvertAll(str.Split(' '), int.Parse);
+
+int posSum = 0;
+for (int i = 0; i < arr.Length; i++)
+    if (arr[i] > 0) posSum++;
+
+Console.WriteLine($"Количество чисел больше 0 : {posSum}");
---
+++ homework43/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+int GetValue(string varname)
+{
+    Console.Write($"Введите значение {varname}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int b1 = GetValue("b1");
+int k1 = GetValue("k1");
+int b2 = GetValue("b2");
+int k2 = GetValue("k2");
+
+double x = (double)(b1 - b2) / (k1 - k2) * -1;
+double y = k2 * x + b2;
+
+Console.WriteLine($"Точка пересечения двух прямых имеет координаты: ({x}; {y})");
+
